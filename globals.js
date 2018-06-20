34d1ec7b60bb967a95abaddc524d237a73520dcf
Jakob-98 <37184788+Jakob-98@users.noreply.github.com>
2018-06-20 22:49:18 +0200

    changed names to f0cus

--- globals.js
+++
@@ -1,8 +0,0 @@
-var
-    PAPER_HEIGHT = 2000,
-    PAPER_WIDTH = Math.round(Math.sqrt(2) * PAPER_HEIGHT),
-    IDF_HEIGHT = 40,
-    IDF_WIDTH = 60,
-    IDF_Xmult = 150,
-    IDF_Ymult = 60,
-    coords = [];
