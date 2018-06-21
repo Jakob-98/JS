80e434d31a6710563e2487cae0c4899517608140
Jakob-98 <37184788+Jakob-98@users.noreply.github.com>
2018-06-21 15:50:46 +0200

    added classes

---
+++ f0cus_classes.js
@@ -0,0 +1,29 @@
+// F0CUS CLASS DEFINITIONS
+
+//CLASS ProcessStep
+class ProcessStep {
+    constructor(name,x,y) {
+        this.name = name;
+        this.x = x;
+        this.y = y;
+    }
+}
+//CLASS ProcessLink
+class ProcessLink {
+    constructor(name,x,y) {
+        this.name = name;
+        this.x = x;
+        this.y = y; 
+    }
+
+}
+
+
+// CLASS MOUSEMANAGER
+class MouseManager {
+    constructor(name) {
+        this.name = name;
+
+    }
+
+}
