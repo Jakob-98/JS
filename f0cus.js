142689ca0e583588fab6e4f9de5bd8f61e5433ab
b3none <ablackham2000@gmail.com>
2018-08-01 14:07:26 +0100

    Removed random whitespace, added semicolons to stop my IDE crying at me.

--- f0cus.js
+++ f0cus.js
@@ -1,41 +1,35 @@
 //main JS file project
 
-
 $(function($){//adding click functionality to the buttons, these clickevents should be moved, but not sure where yet.
     $("#clearbutton").click(function() {
-                paper.clear();
-    })
+        paper.clear();
+    });
     $("#unbindbutton").click(function() {
         unbind();
-    })
+    });
     $("#selectbutton").click(function() {
         unbind();
-        if (SELECT_ELEM == false) {
+        if (SELECT_ELEM === false) {
             SELECT_ELEM = true;
             $("#selectbutton").css("color","blue")
         } 
-    })
+    });
     $("#rectbutton").click(function() {
         unbind();
-        if (DRAW_RECT == false) {
+        if (DRAW_RECT === false) {
             DRAW_RECT = true;
             $("#rectbutton").css("color","blue")
         } 
-    })
+    });
     $("#svg_paper").click(function(e) {
-        if (DRAW_RECT == true) {
+        if (DRAW_RECT === true) {
             crtProcess(MOUSE.posX(e) -30,MOUSE.posY(e)-25 ,RECT_WIDTH,RECT_HEIGHT);
         }
     })
-})
-    
-
+});
 
 //Initialization
 window.onload = function() {
     paper = Raphael("svg_paper", PAPER_HEIGHT, PAPER_WIDTH);
     MOUSE = new MouseManager(); 
-}
-
-
-
+};
