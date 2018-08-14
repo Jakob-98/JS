b4c1ec4c8af0083bf87b80fc8deb1311eb3bc5d4
Jakob <jakobserlier@gmail.com>
2018-08-15 00:22:58 +0200

    Links now connect only to processes, broken the select button TODO fix it. TODO mousemanager is a mess.

--- js/f0cus_classes.js
+++ js/f0cus_classes.js
@@ -70,9 +70,9 @@ class Process { //TO DO: the this.x/y is currently in the left top corner, this 
 		this.shape.push(paper.text(this.x + 3/9 * RECT_WIDTH, this.y + 3/9 * RECT_HEIGHT,'A' + this.stepNr) //TODO add parent "number" -> parent is A1, add 1 to A number 
 			.attr({cursor: 'pointer'}));
 	}
-	onElement () { //returns true if element is under cursor, adds element to selection
-		if (Math.abs(MOUSE.getPosXDown() - this.x) <= 0.5 * RECT_WIDTH &&
-		Math.abs(MOUSE.getPosYDown() - this.y) <= 0.5 * RECT_HEIGHT) {
+	onElement (e) { //returns true if element is under cursor, adds element to selection
+		if (Math.abs(MOUSE.getX(e) - this.x) <= 0.5 * RECT_WIDTH &&
+		Math.abs(MOUSE.getY(e) - this.y) <= 0.5 * RECT_HEIGHT) {
 			ONELEMENT = true; 
 			return this;
 		} else {
@@ -101,6 +101,12 @@ class Link {
 		}
 	}
 	doDraw() {
+		if (this.P1 && this.P2) {
+			this.xEnd = this.P2.x;
+			this.yEnd = this.P2.y;			
+			this.x = this.P1.x;
+			this.y = this.P1.y;
+		}
 		var path = ["M", this.x, this.y, "L", this.xEnd, this.yEnd];
 		if (this === MODEL.activeLink) {
 			this.pathAttr['opacity'] = 0.5;	
@@ -112,6 +118,9 @@ class Link {
 		if (this.shape) this.shape.remove();
 		this.shape = paper.set();
 		this.shape.push(paper.path(path).attr(this.pathAttr));
+	}
+	removeSelf() {
+		MODEL.pLinks.pop(this);
 	}
 }
 //END CLASS Link
@@ -141,18 +150,22 @@ class MouseManager {
 		var offset = $("#svg_paper").offset();
 		return this.pageYUp - PAPER_OFFSET.top;
 	}
-
+	getX(e) {
+		var offset = $("#svg_paper").offset();
+		return e.pageX - PAPER_OFFSET.left;
+	}
+	getY(e) {
+		var offset = $("#svg_paper").offset();
+		return e.pageY - PAPER_OFFSET.top;
+	}
 	mouseDown(e) {
 		this.pageXDown = e.pageX;
 		this.pageYDown = e.pageY;
 
-		if (!SELECT_ELEM) {
-			MODEL.clearSelection();
-		} 
 		
-		if (MODEL.processes.length > 0 && !LINK_ELEM) {
+		if (MODEL.processes.length > 0) {
 			for (var process of MODEL.processes) {
-				process.onElement();
+				process.onElement(e);
 				if (ONELEMENT) {
 					MODEL.selection.push(process);
 					break; //only push one item per selection click
@@ -177,24 +190,42 @@ class MouseManager {
 				MODEL.reDraw();	
 			} 
 			ONELEMENT = false;
+			if (LINK_ELEM) {
+				for (var process of MODEL.processes) {
+					process.onElement(e);
+					if (ONELEMENT) {
+						console.log('test');
+						MODEL.selection.push(process);
+						break; //only push one item per selection click
+					}
+				}
+				if (!ONELEMENT) {
+					MODEL.activeLink.removeSelf();
+			}
+				MODEL.activeLink.P1 = MODEL.selection[0];
+				MODEL.activeLink.P2 = MODEL.selection[1];
+			}
+			ONELEMENT = false;
 			MODEL.activeLink = null;
+			if (!SELECT_ELEM) {
+				MODEL.clearSelection();
+			} 
 			MODEL.reDraw();
 	}
+	
 	mouseMove(e) { 
-		if (!ONELEMENT){
 			if (LINK_ELEM && MODEL.activeLink) {
 				MODEL.activeLink.xEnd = e.pageX - PAPER_OFFSET.left;
 				MODEL.activeLink.yEnd = e.pageY - PAPER_OFFSET.top;
 				MODEL.reDraw();
 			}
-		} else {
-			for (var selected of MODEL.selection) {
-				selected.x = e.pageX + selected.relativePos.x;
-				selected.y = e.pageY + selected.relativePos.y;
+			if (!LINK_ELEM){
+				for (var selected of MODEL.selection) {
+					selected.x = e.pageX + selected.relativePos.x;
+					selected.y = e.pageY + selected.relativePos.y;
+				}
 			}
 			MODEL.reDraw();
 		}
 	}
-
-}
 //END CLASS MouseManager
